47c1a5a58821cb7f2bcc47063125bbd2fd5de650
Erik Öjebo <erik.ojebo@gmail.com>
2013-12-27 19:56:16 +0100

    Initial commit

---
+++ src/GosuArena/GosuArena/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.Mvc;
+using WeenyMapper;
+
+namespace GosuArena.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["GosuArena"];
+            var repository = new Repository();
+
+            return View();
+        }
+    }
+}
