e23c436e5affdb2067debc91994ca58bea2ceb11
Erik Öjebo <erik.ojebo@gmail.com>
2013-12-31 00:34:32 +0100

    Added custom error page

---
+++ src/GosuArena/GosuArena/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace GosuArena.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        [Authorize(Users = "erikojebo")]
+        public ActionResult Create()
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
