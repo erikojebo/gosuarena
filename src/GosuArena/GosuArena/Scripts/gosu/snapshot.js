016751159886f82a8ee6606bdc60ed4fb700353e
Erik Öjebo <erik.ojebo@gmail.com>
2014-01-04 23:33:12 +0100

    merged snapshotting

--- src/GosuArena/GosuArena/Scripts/gosu/snapshot.js
+++ src/GosuArena/GosuArena/Scripts/gosu/snapshot.js
@@ -5,23 +5,36 @@ gosu.snapshot.extend = function (obj) {
 
     var snapshotValues = {};
 
+    function snapshotSelectedProperties(propertyNames) {
+        for (var i = 0; i < propertyNames.length; i++) {
+            snapshotValues[propertyNames[i]] = obj[propertyNames[i]];
+        }
+    }
+
+    function snapshotAllOwnProperties() {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                snapshotValues[prop] = obj[prop];
+            }
+        }
+    }
+
     obj.snapshot = function () {
 
         snapshotValues.length = 0;
-        
-        for (var prop in obj) {
-            if (obj.hasOwnProperty(prop)) {
-                snapshotValues[prop] = obj[prop];                
-            }
+
+        if (arguments.length > 0) {
+            snapshotSelectedProperties(arguments);
+        } else {
+            snapshotAllOwnProperties();
         }
     };
 
     obj.restoreSnapshot = function () {
         for (var prop in snapshotValues) {
             if (snapshotValues.hasOwnProperty(prop)) {
-                obj[prop] = snapshotValues[prop];                
+                obj[prop] = snapshotValues[prop];
             }
         }
     };
 }
-
