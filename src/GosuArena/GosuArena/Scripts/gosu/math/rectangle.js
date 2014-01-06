b82b9a7e64f10462b4f633c7b992915afe092ee8
Erik Öjebo <erik.ojebo@gmail.com>
2014-01-06 13:47:54 +0100

    Rewrite implementation details of rectangle to improve performance when creating new rectangles

--- src/GosuArena/GosuArena/Scripts/gosu/math/rectangle.js
+++ src/GosuArena/GosuArena/Scripts/gosu/math/rectangle.js
@@ -8,25 +8,44 @@ gosu.math.rectangle = gosu.math.rectangle || {};
 
         var corners = clockwiseCorners;
 
-        function sortNumber(a, b) {
-            return a - b;
+        // This was previously written as corners.map(...)
+        // but was re-written for performance.
+        var cornerXValues = new Array(4);
+        var cornerYValues = new Array(4);
+
+        for (var i = 0; i < 4; i++) {
+            cornerXValues[i] = corners[i].x;
+            cornerYValues[i] = corners[i].y;
         }
 
-        var sortedCornerXValues = corners.map(function (corner) {
-            return corner.x;
-        }).sort(sortNumber);
+        // This was previously written as Math.min/max
+        // but was rewritten inline to improve performance
+        var minX = cornerXValues[0];
+        var maxX = cornerXValues[0];
+        var minY = cornerYValues[0];
+        var maxY = cornerYValues[0];
 
-        var sortedCornerYValues = corners.map(function (corner) {
-            return corner.y;
-        }).sort(sortNumber);
-        
-        var minX = sortedCornerXValues[0];
-        var maxX = sortedCornerXValues[corners.length - 1];
-        var minY = sortedCornerYValues[0];
-        var maxY = sortedCornerYValues[corners.length - 1];
+        // Since all values are initialized with the first value,
+        // skip the first value when comparing
+        for (var i = 1; i < 4; i++) {
+
+            if (cornerXValues[i] < minX) {
+                minX = cornerXValues[i];
+            } else if (cornerXValues[i] > maxX) {
+                maxX = cornerXValues[i];
+            }
+
+            if (cornerYValues[i] < minY) {
+                minY = cornerYValues[i];
+            } else if (cornerYValues[i] > maxY) {
+                maxY = cornerYValues[i];
+            }
+        }
 
         var center = { x: (maxX - minX) / 2 + minX, y: (maxY - minY) / 2 + minY };
 
+        // This was previously extracted to a separate function, but was
+        // inlined for performance
         var edges = [];
 
         for (var i = 0; i < 4; i++) {
@@ -58,13 +77,17 @@ gosu.math.rectangle = gosu.math.rectangle || {};
                         otherRectangle.edges[j])) {
 
                         intersectingLineCount++;
+
+                        if (intersectingLineCount >= 2) {
+                            return true;
+                        }
                     }
                 }
             }
 
             var isOtherRectangleInsideThisOne =
                 gosu.math.isPointInsideEdges(otherRectangle.corners[0], edges);
-            
+
             var isThisRectangleInsideOther =
                 gosu.math.isPointInsideEdges(corners[0], otherRectangle.edges);
 
@@ -77,7 +100,7 @@ gosu.math.rectangle = gosu.math.rectangle || {};
         }
 
         function rotateAroundCenter(degrees) {
-             return rotate(degrees, center);
+            return rotate(degrees, center);
         }
 
         function rotate(degrees, rotationCenter) {
@@ -99,7 +122,7 @@ gosu.math.rectangle = gosu.math.rectangle || {};
             maxX: maxX,
             minY: minY,
             maxY: maxY
-        };        
+        };
     }
 
     gosu.math.rectangle.createFromPoints = function (points) {
